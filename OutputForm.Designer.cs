6ca86fba06c9702983e3564d9fc49ee5df6a22e3
Elvis Abene <abene197@gmail.com>
2021-05-05 21:59:15 +0300

    commit2

---
+++ OutputForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Transport_Task
+{
+    partial class OutputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputLabel.Location = new System.Drawing.Point(246, 19);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(583, 378);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // OutputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 638);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "OutputForm";
+            this.Text = "OutputForm";
+            this.Load += new System.EventHandler(this.OutputForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
---
+++ OutputForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Transport_Task.TaskAbstractions;
+
+namespace Transport_Task
+{
+    public partial class OutputForm : Form
+    {
+        InputForm parent;
+        public OutputForm(InputForm parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        public void ShowResult(List<string> result)
+        {
+            this.Show();
+            foreach (string str in result)
+            {
+                outputLabel.Text += "\n" + str;
+            }
+        }
+
+        private void OutputForm_Load(object sender, EventArgs e)
+        {
+            Solver solver = new Solver(parent);
+            ShowResult(solver.FinalResult());
+        }
+    }
+}
